0358860a29e56c7a56c2ead979a46e53d82129d5
Chue Chang <chuechang1996@outlook.com>
2022-02-04 19:13:13 -0600

    created routes folder, htmlRoute file coded

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const htmlPath = require('express').Router();
+const path = require('path');
+
+htmlPath.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+htmlPath.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+module.exports = htmlPath;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const htmlRoute = require('./routes/htmlRoutes');
 const fs = require('fs');
 
 const PORT = 3001;
@@ -9,16 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.use(htmlRoute)
 
 
+// Listening to PORT
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
