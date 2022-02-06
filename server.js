29d54e854a92049ef82c8fe166d4305d8301db0b
Chue Chang <chuechang1996@outlook.com>
2022-02-06 15:36:04 -0600

    added PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlRoute = require('./routes/htmlRoutes');
 const apiRoute = require('./routes/apiRoutes')
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
